b8de601168bdcb04510631b2d4716d748d8181bd
RoskiDeluge <roberto.delgado@gmail.com>
2020-01-27 16:09:38 -1000

    Working on fetch request

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    players: [],
+    error: ""
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:3000/api/players")
+      .then(res => res.json())
+      .then(player => {
+        console.log("fetch: players: ", player);
+        this.setState({ ...this.state, players: player });
+      })
+      .catch(err => console.log("error on fetch: ", err));
+  };
+
+  // handleDogTextChange = e => {
+  //   this.setState({ ...this.state, doggoText: e.target.value });
+  // };
+
+  // handleDogFetch = e => {
+  //   e.preventDefault();
+  //   fetch(`https://dog.ceo/api/breed/${this.state.doggoText}/images`)
+  //     .then(res => res.json())
+  //     .then(dogData => {
+  //       console.log("handleDogFetch: dogData: ", dogData);
+  //       if (dogData.status !== "error") {
+  //         this.setState({ ...this.state, doggos: dogData.message });
+  //       }
+  //     })
+  //     .catch(err => console.log("fethc in handleDogFetch error: err:", err));
+  // };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Women's World Cup Players</h1>
+        <div className="doggos">
+          {this.state.players &&
+            this.state.players.map(player => {
+              return <p>{player.name}</p>;
+            })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
